3c2f2bc3d5e8277c377d8e60e1b251eeafb2e4b0
Petr Šplíchal <psplicha@redhat.com>
2018-11-05 16:15:17 +0100

    Remove trailing white space

--- source/xmlrpc.py
+++ source/xmlrpc.py
@@ -47,10 +47,10 @@ class CookieResponse:
     '''Fake HTTPResponse object that we can fill with headers we got elsewhere.
     We can then pass it to CookieJar.extract_cookies() to make it pull out the
     cookies from the set of headers we have.'''
-    def __init__(self,headers): 
+    def __init__(self,headers):
         self.headers = headers
         #log.debug("CookieResponse() headers = %s" % headers)
-    def info(self): 
+    def info(self):
         return self.headers
 
 
@@ -58,8 +58,8 @@ class CookieTransport(xmlrpclib.Transport):
     '''A subclass of xmlrpclib.Transport that supports cookies.'''
     cookiejar = None
     scheme = 'http'
-    
-    # Cribbed from xmlrpclib.Transport.send_user_agent 
+
+    # Cribbed from xmlrpclib.Transport.send_user_agent
     def send_cookies(self, connection, cookie_request):
         if self.cookiejar is None:
             self.cookiejar = CookieJar()
@@ -74,7 +74,7 @@ class CookieTransport(xmlrpclib.Transport):
             # ...and put them over the connection
             for h,v in cookielist:
                 connection.putheader(h,v)
-    
+
     # This is the same request() method from xmlrpclib.Transport,
     # with a couple additions noted below
     def request_with_cookies(self, host, handler, request_body, verbose=0):
@@ -85,10 +85,10 @@ class CookieTransport(xmlrpclib.Transport):
         # ADDED: construct the URL and Request object for proper cookie handling
         request_url = "%s://%s%s" % (self.scheme,host,handler)
         #log.debug("request_url is %s" % request_url)
-        cookie_request  = urllib2.Request(request_url) 
+        cookie_request  = urllib2.Request(request_url)
 
         self.send_request(h,handler,request_body)
-        self.send_host(h,host) 
+        self.send_host(h,host)
         self.send_cookies(h,cookie_request) # ADDED. creates cookiejar if None.
         self.send_user_agent(h)
         self.send_content(h,request_body)
@@ -202,10 +202,10 @@ class SafeCookieTransport(xmlrpclib.SafeTransport,CookieTransport):
 # Ported to use python-gssapi
 class GSSAPITransport(SafeCookieTransport):
     """Handles GSSAPI Negotiation (SPNEGO) authentication to an XML-RPC server."""
-    
+
     def get_host_info(self, host):
         host, extra_headers, x509 = xmlrpclib.Transport.get_host_info(self, host)
-        
+
         # Set the remote host principal
         h = host
         hostinfo = h.split(':')
@@ -214,11 +214,11 @@ class GSSAPITransport(SafeCookieTransport):
         service_name = gssapi.Name(service, gssapi.NameType.hostbased_service)
         vc = gssapi.SecurityContext(usage="initiate", name=service_name)
         response = vc.step()
-        
+
         extra_headers = [
             ("Authorization", "negotiate %s" % b64encode(response).decode() )
         ]
-        
+
         return host, extra_headers, x509
 
     def _python_ver_larger_than_2_6(self):
@@ -263,7 +263,7 @@ class NitrateXmlrpcError(Exception):
         self.verb = verb
         self.params = params
         self.wrappedError = wrappedError
-    
+
     def __str__(self):
         return "Error while executing cmd '%s' --> %s" \
                % ( self.verb + "(" + self.params + ")", self.wrappedError)
@@ -283,9 +283,9 @@ class NitrateXmlrpc(object):
                 'username', 'password', 'url'
             ]]
         )
-        
+
         return NitrateXmlrpc(**kwargs)
-    
+
     def __init__(self, username, password, url, use_mod_auth_kerb = False):
         if url.startswith('https://'):
             self._transport = SafeCookieTransport()
@@ -293,7 +293,7 @@ class NitrateXmlrpc(object):
             self._transport = CookieTransport()
         else:
             raise NitrateError("Unrecognized URL scheme")
-        
+
         self._transport.cookiejar = CookieJar()
         # print "COOKIES:", self._transport.cookiejar._cookies
         self.server = xmlrpclib.ServerProxy(
@@ -302,21 +302,21 @@ class NitrateXmlrpc(object):
             verbose = VERBOSE,
             allow_none = 1
         )
-        
+
         # Login, get a cookie into our cookie jar:
         login_dict = self.do_command("Auth.login", [dict(
                 username = username,
                 password = password,
         )])
-        
+
         # Record the user ID in case the script wants this
         # self.user_id = login_dict['id']
         # print 'Logged in with cookie for user %i' % self.userId
         # print "COOKIES:", self._transport.cookiejar._cookies
-    
+
     def _boolean_option(self, option, value):
         """Returns the boolean option when value is True or False, else ''
-        
+
         Example: _boolean_option('isactive', True) returns " 'isactive': 1,"
         """
         if value or str(value) == 'False':
@@ -327,11 +327,11 @@ class NitrateXmlrpc(object):
             elif value == True:
                 return "\'%s\':1, " % option
         return ''
-    
+
     def _datetime_option(self, option, value):
         """Returns the string 'option': 'value' where value is a date object formatted
         in string as yyyy-mm-dd hh:mm:ss. If value is None, then we return ''.
-        
+
         Example: self._time_option('datetime', datetime(2007,12,05,13,01,03))
         returns "'datetime': '2007-12-05 13:01:03'"
         """
@@ -340,11 +340,11 @@ class NitrateXmlrpc(object):
                 raise NitrateError("The option '%s' is not a valid datetime object." % option)
             return "\'%s\':\'%s\', " % (option, value.strftime("%Y-%m-%d %H:%M:%S"))
         return ''
-    
+
     def _list_dictionary_option(self, option, value):
         """Verifies that the value passed for the option is in the format of a list
         of dictionaries.
-        
+
         Example: _list_dictionary_option('plan':[{'key1': 'value1', 'key2': 'value2'}])
         verifies that value is a list, then verifies that the content of value are dictionaries.
         """
@@ -357,12 +357,12 @@ class NitrateXmlrpc(object):
                         raise NitrateError("The option '%s' is not a valid list of dictionaries." % option)
             return "\'%s\': %s" % (option, value)
         return ''
-    
+
     _list_dict_op = _list_dictionary_option
-    
+
     def _number_option(self, option, value):
         """Returns the string " 'option': value," if value is not None, else ''
-        
+
         Example: self._number_option("isactive", 1) returns " 'isactive': 1,"
         """
         if value:
@@ -370,30 +370,30 @@ class NitrateXmlrpc(object):
                 raise NitrateError("The option '%s' is not a valid integer." % option)
             return "\'%s\':%d, " % (option, value)
         return ''
-    
+
     def _number_no_option(self, number):
         """Returns the number in number. Just a totally useless wrapper :-)
-        
+
         Example: self._number_no_option(1) returns 1
         """
         if type(number) is not IntType:
             raise NitrateError("The 'number' parameter is not an integer.")
         return str(number)
-    
+
     _number_noop = _number_no_option
-    
+
     def _options_dict(self, *args):
         """Creates a wrapper around all the options into a dictionary format.
-        
+
         Example, if args is ['isactive': 1,", 'description', 'Voyage project'], then
         the return will be {'isactive': 1,", 'description', 'Voyage project'}
         """
         return "{%s}" % ''.join(args)
-        
+
     def _options_non_empty_dict(self, *args):
         """Creates a wrapper around all the options into a dictionary format and
         verifies that the dictionary is not empty.
-        
+
         Example, if args is ['isactive': 1,", 'description', 'Voyage project'], then
         the return will be {'isactive': 1,", 'description', 'Voyage project'}.
         If args is empty, then we raise an error.
@@ -401,12 +401,12 @@ class NitrateXmlrpc(object):
         if not args:
             raise NitrateError("At least one variable must be set.")
         return "{%s}" % ''.join(args)
-    
+
     _options_ne_dict = _options_non_empty_dict
-    
+
     def _string_option(self, option, value):
         """Returns the string 'option': 'value'. If value is None, then ''
-        
+
         Example: self._string_option('description', 'Voyage project') returns
         "'description' : 'Voyage project',"
         """
@@ -415,10 +415,10 @@ class NitrateXmlrpc(object):
                 raise NitrateError("The option '%s' is not a valid string." % option)
             return "\'%s\':\'%s\', " % (option, value)
         return ''
-    
+
     def _string_no_option(self, option):
         """Returns the string 'option'.
-        
+
         Example: self._string_no_option("description") returns "'description'"
         """
         if option:
@@ -426,13 +426,13 @@ class NitrateXmlrpc(object):
                 raise NitrateError("The option '%s' is not a valid string." % option)
             return "\'%s\'" % option
         return ''
-    
+
     _string_noop = _string_no_option
-    
+
     def _time_option(self, option, value):
         """Returns the string 'option': 'value' where value is a time object formatted in string as hh:mm:ss.
         If value is None, then we return ''.
-        
+
         Example: self._time_option('time', time(12,00,03)) returns "'time': '12:00:03'"
         """
         if value:
@@ -440,10 +440,10 @@ class NitrateXmlrpc(object):
                 raise NitrateError("The option '%s' is not a valid time object." % option)
             return "\'%s\':\'%s\', " % (option, value.strftime("%H:%M:%S"))
         return ''
-    
+
     def do_command(self, verb, args = []):
         """Submit a command to the server proxy.
-        
+
         'verb' -- string, the xmlrpc verb,
         'args' -- list, the argument list,
         """
@@ -453,30 +453,30 @@ class NitrateXmlrpc(object):
         cmd = "self.server." + verb + "(" + params + ")"
         if DEBUG:
             print cmd
-        
+
         try:
             return eval(cmd)
         except xmlrpclib.Error, e:
             raise NitrateXmlrpcError(verb, params, e)
-    
+
     ############################## Build #######################################
-    
+
     def build_get(self, build_id):
         """Get A Build by ID.
-        
+
         'build_id' -- integer, Must be greater than 0
-        
+
         Example: build_get(10)
-        
+
         Result: A dictionary of key/value pairs for the attributes listed above
         """
         return self.do_command("Build.get", [self._number_noop(build_id)])
-    
+
     ############################## User ##################################
     def get_me(self):
         """
         Description: Get the information of myself
-        
+
         Returns:     A blessed User object Hash
         """
         return self.do_command("User.get_me")
@@ -494,7 +494,7 @@ class NitrateKerbXmlrpc(NitrateXmlrpc):
                     "GSSAPI authentication.\nURL provided: {0}".format(url))
         else:
             raise NitrateError("Unrecognized URL scheme: {0}".format(url))
-        
+
         self._transport.cookiejar = CookieJar()
         # print "COOKIES:", self._transport.cookiejar._cookies
         self.server = xmlrpclib.ServerProxy(
@@ -503,7 +503,7 @@ class NitrateKerbXmlrpc(NitrateXmlrpc):
             verbose = VERBOSE,
             allow_none = 1
         )
-        
+
         # Login, get a cookie into our cookie jar:
         login_dict = self.do_command("Auth.login_krbv", [])
 
