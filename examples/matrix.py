e0fe576c3b4a1d74cdca7298e2cee7beb7344e3b
Štěpán Němec <snemec@redhat.com>
2021-03-02 16:26:23 +0100

    Make examples usable with Python 3
    That seems to have been the intention, as the shebang already assumes
    python3.
    
    I haven't tried actually running create.py (it looks more like just
    a collection of examples), but matrix.py now does work as a python3
    script.
    
    Fixes: 90b1d68227df ("Migrate the code to Python 3")

--- examples/matrix.py
+++ examples/matrix.py
@@ -9,15 +9,15 @@ if __name__ == "__main__":
     options = parser.parse_args()[0]
 
     testplan = TestPlan(options.plan)
-    print "Checking results of {0}:\nTest runs: {1}".format(testplan,
-            ", ".join([run.identifier for run in testplan.testruns]))
+    print("Checking results of {0}:\nTest runs: {1}".format(testplan,
+            ", ".join([run.identifier for run in testplan.testruns])))
 
-    for testcase in sorted(testplan.testcases, key=lambda x: x.script):
+    for testcase in sorted(testplan.testcases, key=lambda x: x.script or ""):
         for testrun in testplan.testruns:
             for caserun in testrun.caseruns:
                 if caserun.testcase.id == testcase.id:
-                    print caserun.status.shortname,
+                    print(caserun.status.shortname, end=" ")
                     break
             else:
-                print "....",
-        print "- {0} - {1}".format(str(testcase.tester).ljust(8), testcase)
+                print("....", end=" ")
+        print("- {0} - {1}".format(str(testcase.tester).ljust(8), testcase))
